6f51e1c23227af60b4ce35bc03d97fdf945824da
Franco Mamani <thefk2000@gmail.com>
2024-04-02 21:17:40 -0300

    service

--- src/main/java/ar/edu/unq/eperdemic/services/impl/PatogenoServiceImpl.kt
+++ src/main/java/ar/edu/unq/eperdemic/services/impl/PatogenoServiceImpl.kt
@@ -3,24 +3,29 @@ package ar.edu.unq.eperdemic.services.impl
 import ar.edu.unq.eperdemic.modelo.Especie
 import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.persistencia.dao.PatogenoDAO
+import ar.edu.unq.eperdemic.persistencia.dao.jdbc.JDBCPatogenoDAO
 import ar.edu.unq.eperdemic.services.PatogenoService
 
 
 class PatogenoServiceImpl(val patogenoDAO: PatogenoDAO) : PatogenoService {
-
+    private val dao: PatogenoDAO = JDBCPatogenoDAO()
     override fun crearPatogeno(patogeno: Patogeno): Patogeno {
-        TODO("not implemented")
+        return dao.crear(patogeno)
     }
 
     override fun recuperarPatogeno(id: Long): Patogeno {
-        TODO("not implemented")
+        return dao.recuperar(id)
     }
 
     override fun recuperarATodosLosPatogenos(): List<Patogeno> {
-        TODO("not implemented")
+        return dao.recuperarATodos()
     }
 
     override fun agregarEspecie(idDePatogeno: Long, nombreEspecie: String, paisDeOrigen: String): Especie {
-        TODO("not implemented")
+        val patogeno: Patogeno = dao.recuperar(idDePatogeno)
+        val especie: Especie = patogeno.crearEspecie(nombreEspecie, paisDeOrigen)
+        dao.actualizar(patogeno)
+        return especie
     }
+
 }
