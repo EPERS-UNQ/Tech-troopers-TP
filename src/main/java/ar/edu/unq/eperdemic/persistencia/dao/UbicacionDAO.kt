64fe5440548355a4142465c07094aa12e06fd7aa
Franco Mamani <thefk2000@gmail.com>
2024-04-26 01:27:47 -0300

    fixes

--- src/main/java/ar/edu/unq/eperdemic/persistencia/dao/UbicacionDAO.kt
+++ src/main/java/ar/edu/unq/eperdemic/persistencia/dao/UbicacionDAO.kt
@@ -1,19 +1,12 @@
 package ar.edu.unq.eperdemic.persistencia.dao
 
-import ar.edu.unq.eperdemic.modelo.Patogeno
 import ar.edu.unq.eperdemic.modelo.Ubicacion
-import org.springframework.data.repository.CrudRepository
 
-interface UbicacionDAO : CrudRepository<Patogeno, Long> {
-
+interface UbicacionDAO {
     fun crear(ubicacion: Ubicacion): Ubicacion
-
     fun actualizar(ubicacion: Ubicacion)
-
     fun recuperar(id: Long?): Ubicacion
-
     fun recuperarTodos(): List<Ubicacion>
-
     fun cantidadDeUbicaciones(): Int
     fun recuperarPorNombre(ubicacionId: Long?): String
 
