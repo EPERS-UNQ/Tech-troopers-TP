7f42cab58a8a2ea791986ffe03d3585b0ff4de04
Cirilli, Ignacio <88701429+IgnacioCirilli@users.noreply.github.com>
2024-04-30 13:32:22 -0300

    creacion de UbicacionDTO y UbicacionControllerREST

---
+++ src/main/java/ar/edu/unq/eperdemic/controller/dto/UbicacionDTO.kt
@@ -0,0 +1,24 @@
+package ar.edu.unq.eperdemic.controller.dto
+
+import ar.edu.unq.eperdemic.modelo.Ubicacion
+
+class UbicacionDTO ( val id: Long?,
+                     val nombre: String? ) {
+
+    companion object {
+        fun desdeModelo(ubicacion: Ubicacion) =
+            UbicacionDTO(
+                id           = ubicacion.getId(),
+                nombre       = ubicacion.getNombre()
+            )
+    }
+
+    fun aModelo(): Ubicacion {
+        val ubicacion = Ubicacion()
+        // ubicacion.id  = this.id  // se deberia hacer un setter o no se deberia colocar, ya que se genera automaticamente en el back?
+        ubicacion.setNombre(this.nombre!!)
+        return ubicacion
+    }
+
+
+}
